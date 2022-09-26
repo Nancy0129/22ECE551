9e1e5034e1eb98be95ccd2cd131e0c3e3b2b87f4
xc139 <xc139@duke.edu>
2022-09-25 20:11:34 -0400

    041

--- 041_reverse_str/reverse.c
+++ 041_reverse_str/reverse.c
@@ -4,6 +4,22 @@
 
 void reverse(char * str) {
   //WRITE ME!
+  int n = 0;
+  char tem;
+
+  while (str[n] != 0) {
+    n++;
+  }
+  if (n <= 1) {
+    return;
+  }
+
+  for (int i = 0; i < n / 2; i++) {
+    tem = str[i];
+    str[i] = str[n - 1 - i];
+    str[n - 1 - i] = tem;
+  }
+  return;
 }
 
 int main(void) {
@@ -13,7 +29,8 @@ int main(void) {
   char str3[] = "Captain's log, Stardate 42523.7";
   char str4[] = "Hello, my name is Inigo Montoya.";
   char str5[] = "You can be my wingman anyday!";
-  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be "
+                "no tomorrow!";
   char * array[] = {str0, str1, str2, str3, str4, str5, str6};
   for (int i = 0; i < 7; i++) {
     reverse(array[i]);
