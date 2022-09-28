e25d751ebb7adacf063a6f057455360335dc97d0
xc139 <xc139@duke.edu>
2022-09-28 14:51:33 -0400

    add more tests

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -6,24 +6,23 @@ int readMax(FILE * f, char array[10][10]) {
   char c;
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
-      c = fgetc(f);
-      if (c == EOF) {
-        fprintf(stderr, "Too short text!");
+      if ((c = fgetc(f)) == EOF) {
+        fprintf(stderr, "Too short text!\n");
         return EXIT_FAILURE;
       }
       if (c == '\n') {
-        fprintf(stderr, "Too short row");
+        fprintf(stderr, "Too short row\n");
         return EXIT_FAILURE;
       }
       array[j][9 - i] = c;
     }
     if ((c = fgetc(f)) != '\n') {
-      fprintf(stderr, "Too long row");
+      fprintf(stderr, "Too long row\n");
       return EXIT_FAILURE;
     }
   }
   if ((c = fgetc(f)) != EOF) {
-    fprintf(stderr, "Too long texts!");
+    fprintf(stderr, "Too long texts!\n");
     return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
@@ -34,14 +33,16 @@ int main(int argc, char ** argv) {
     fprintf(stderr, "Usage: encrypt key inputFileName\n");
     return EXIT_FAILURE;
   }
-
   FILE * f = fopen(argv[1], "r");
   if (f == NULL) {
-    perror("Could not open file");
+    perror("Could not open file!\n");
     return EXIT_FAILURE;
   }
   char array[10][10];
-  readMax(f, array);
+  int out = readMax(f, array);
+  if (out != EXIT_SUCCESS) {
+    return EXIT_FAILURE;
+  }
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
       printf("%c", array[i][j]);
@@ -50,7 +51,7 @@ int main(int argc, char ** argv) {
   }
 
   if (fclose(f) != 0) {
-    perror("Failed to close the input file!");
+    perror("Failed to close the input file!\n");
     return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
