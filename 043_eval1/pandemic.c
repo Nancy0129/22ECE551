c9356667e933210b1ebc0b0ed5ddb74d2179d746
xc139 <xc139@duke.edu>
2022-09-29 00:11:00 -0400

    version 1

--- 043_eval1/pandemic.c
+++ 043_eval1/pandemic.c
@@ -1,10 +1,64 @@
 #include "pandemic.h"
 
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 country_t parseLine(char * line) {
   //WRITE ME
+  if (line == NULL) {
+    fprintf(stderr, "Invalid Input in 1: got unexpected NULL!\n");
+    exit(EXIT_FAILURE);
+  }
+  if (*line == '\0' || *line == '\n') {
+    fprintf(stderr, "Invalid Input in 1: line is too short!\n");
+    exit(EXIT_FAILURE);
+  }
   country_t ans;
   ans.name[0] = '\0';
   ans.population = 0;
+  int nameL = 0;
+  int popuL = 0;
+  char max_uin64[] = "18446744073709551615";
+  while (line[nameL] != ',') {
+    if (line[nameL] == '\n' || line[nameL] == '\0') {
+      fprintf(stderr, "Invalid Input in 1: expect comma but did not find!\n");
+      exit(EXIT_FAILURE);
+    }
+    if (nameL > 63) {
+      fprintf(stderr, "Invalid Input in 1: line is too long!\n");
+      exit(EXIT_FAILURE);
+    }
+    ans.name[nameL] = line[nameL];
+    nameL++;
+  }
+  nameL++;
+  while (line[nameL + popuL] != '\n' || line[nameL + popuL] != '\0') {
+    if (line[nameL + popuL] < 48 || line[nameL + popuL] > 57) {
+      fprintf(stderr, "Invalid Input in 1: invalid character for population!\n");
+      exit(EXIT_FAILURE);
+    }
+    if (popuL > 19) {
+      fprintf(stderr, "Invalid Input in 1: too large  number!\n");
+      exit(EXIT_FAILURE);
+    }
+    if (popuL == 19) {
+      for (int i = 0; i < 20; i++) {
+        if (line[nameL + i] > max_uin64[i]) {
+          fprintf(stderr, "Invalid Input in 1: too large  number!\n");
+          exit(EXIT_FAILURE);
+        }
+      }
+    }
+    popuL++;
+  }
+  if (popuL < 1) {
+    fprintf(stderr, "Invalid Input in 1: no number input!\n");
+    exit(EXIT_FAILURE);
+  }
+  // The input for strtoull to store invalid characters
+  char * endptr;
+  ans.population = strtoull(line + nameL, &endptr, 10);
   return ans;
 }
 
---
+++ 043_eval1/try.c
@@ -0,0 +1,23 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pandemic.h"
+int main(void) {
+  char word[] = "xyz,18446744073709551616";
+  int i = 0;
+  while (word[i] != ',') {
+    i++;
+  }
+  char * nums = word + i + 1;
+  char * pr;
+  uint64_t a = strtoull(nums, &pr, 10);
+
+  if (*pr != '\0') {
+    printf("%d\n", 1);
+  }
+  printf("%lu\n", a);
+  printf("%s\n", pr);
+  printf("%s\n", nums);
+  return 0;
+}
