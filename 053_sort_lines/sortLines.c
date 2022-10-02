40a80b1b96aaed144d914ee122fa3ed5e4e9c231
xc139 <xc139@duke.edu>
2022-10-02 12:52:15 -0400

    053

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,8 +14,57 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
-int main(int argc, char ** argv) {
-  //WRITE YOUR CODE HERE!
+void showRes(char ** lines, size_t len) {
+  for (size_t i = 0; i < len; i++) {
+    printf("%s", lines[i]);
+    free(lines[i]);
+  }
+  free(lines);
+  return;
+}
 
+int main(int argc, char ** argv) {
+  size_t len = 0;
+  size_t data_len = 0;
+  char * data = NULL;
+  char ** lines = NULL;
+  FILE * f;
+  if (argc == 1) {
+    while (getline(&data, &data_len, stdin) >= 0) {
+      if (data[0] == '\n' || data[0] == '\0') {
+        break;
+      }
+      lines = realloc(lines, (len + 1) * sizeof(*lines));
+      lines[len] = data;
+      data = NULL;
+      len++;
+    }
+    free(data);
+    sortData(lines, len);
+    showRes(lines, len);
+  }
+  else {
+    for (int i = 1; i < argc; i++) {
+      f = fopen(argv[i], "r");
+      if (f == NULL) {
+        perror("Could not open file!\n");
+        return EXIT_FAILURE;
+      }
+      while (getline(&data, &data_len, f) >= 0) {
+        lines = realloc(lines, (len + 1) * sizeof(*lines));
+        lines[len] = data;
+        data = NULL;
+        len++;
+      }
+      free(data);
+      sortData(lines, len);
+      showRes(lines, len);
+      if (fclose(f) != 0) {
+        perror("Failed to close the input file!\n");
+
+        return EXIT_FAILURE;
+      }
+    }
+  }
   return EXIT_SUCCESS;
 }
