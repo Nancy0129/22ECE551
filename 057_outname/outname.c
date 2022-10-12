c2d4e9a1ef2fd34820eb7bc028deb8f93952fa9f
xc139 <xc139@duke.edu>
2022-10-12 00:32:14 -0400

    057

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,25 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  char * outName = NULL;
+  int j = 0;
+  while (inputName[j] != '\0') {
+    outName = realloc(outName, (j + 1) * sizeof(*outName));
+    outName[j] = inputName[j];
+    j++;
+  }
+  if (inputName[j - 1] == 't' && inputName[j - 2] == 'x' && inputName[j - 3] == 't' &&
+      inputName[j - 4] == '.') {
+    char count[8] = ".counts";
+    outName = realloc(outName, (j + 8) * sizeof(*outName));
+    for (int i = 0; i < 8; i++) {
+      outName[j + i] = count[i];
+    }
+    return outName;
+  }
+  return NULL;
 }
