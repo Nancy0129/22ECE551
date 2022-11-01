b982f477d4cbc5ce4602b32a5b62df7b14b64a54
xc139 <xc139@duke.edu>
2022-11-01 00:24:44 -0400

    083

---
+++ 083_expr1/expr.hpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <sstream>
+#include <string>
+using namespace std;
+class Expression {
+ public:
+  Expression() {}
+  virtual string toString() const = 0;
+  virtual ~Expression() {}
+};
+class NumExpression : public Expression {
+  long n;
+
+ public:
+  NumExpression(long num) : n(num) {}
+  string toString() const {
+    stringstream a;
+    a << n;
+    return a.str();
+  }
+  ~NumExpression() {}
+};
+class PlusExpression : public Expression {
+  Expression * left;
+  Expression * right;
+
+ public:
+  PlusExpression(Expression * lhs, Expression * rhs) : left(lhs), right(rhs) {}
+  string toString() const {
+    stringstream expr;
+    expr << "(";
+    expr << left->toString();
+    expr << " + ";
+    expr << right->toString();
+    expr << ")";
+    return expr.str();
+  }
+  ~PlusExpression(){};
+};
