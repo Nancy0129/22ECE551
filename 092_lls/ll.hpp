c993654d54176a63acd0d49490644808500e8b60
xc139 <xc139@duke.edu>
2022-11-06 22:02:06 -0500

    092

--- 092_lls/ll.hpp
+++ 092_lls/ll.hpp
@@ -5,7 +5,137 @@
 
 #include <cstdlib>
 #include <exception>
+template<typename T>
+class LinkedList {
+ private:
+  class Node {
+   public:
+    T data;
+    Node * next;
+    Node * prev;
+    Node(const T & d) : data(d), next(NULL), prev(NULL) {}
+    Node(const T & d, Node * n, Node * p) : data(d), next(n), prev(p) {}
+  };
+  Node * head;
+  Node * tail;
+  int size;
 
-//YOUR CODE GOES HERE
+  Node * remove(const T & data, Node * current) {
+    if (current == NULL) {
+      return NULL;
+    }
+    if (data == current->data) {
+      Node * ans = current->next;
+      if (current->next == NULL) {
+        tail = current->prev;
+      }
+      else {
+        current->next->prev = current->prev;
+      }
+      delete current;
+      return ans;
+    }
+    current->next = remove(data, current->next);
+    return current;
+  }
+
+ public:
+  LinkedList() : head(NULL), tail(NULL), size(0) {}
+  void addFront(const T & item) {
+    Node toAdd(item);
+    if (head != NULL) {
+      toAdd.next = head;
+      head->pre = &toAdd;
+    }
+    head = &toAdd;
+    if (tail == NULL) {
+      tail = &toAdd;
+    }
+    size++;
+  }
+  void addBack(const T & item) {
+    Node toAdd(item);
+    if (tail != NULL) {
+      toAdd.prev = tail;
+      tail->next = &toAdd;
+    }
+    tail = &toAdd;
+    if (head == NULL) {
+      head = &toAdd;
+    }
+    size++;
+  }
+  bool remove(const T & item) {
+    Node * res = remove(item, head);
+    if (res == NULL) {
+      return false;
+    }
+    head = res;
+    size--;
+    return true;
+  }
+  T & operator[](int index) {
+    assert(index >= 0 && index < size);
+    Node * curr = head;
+    for (int i = 0; i < index; i++) {
+      curr = curr->next;
+    }
+    return curr->data;
+  }
+  int find(const T & item) const {
+    Node * curr = head;
+    int ind = 0;
+    while (curr != NULL) {
+      if (curr->data == item) {
+        return ind;
+      }
+      curr = curr->next;
+      ind++;
+    }
+    return -1;
+  }
+  ~LinkedList() {
+    while (head != NULL) {
+      Node * temp = head->next;
+      delete head;
+      head = temp;
+    }
+  }
+  LinkedList(const LinkedList & rhs) : head(NULL), tail(NULL), size(rhs.size) {
+    if (rhs.size > 0) {
+      Node * curr = rhs.head;
+      while (curr != NULL) {
+        this->addBack(curr->data);
+        curr = curr->next;
+      }
+    }
+  }
+
+  LinkedList & operator=(const LinkedList & rhs) {
+    if (this != rhs) {
+      Node h(rhs.head->data);
+      Node * rCurr = rhs.head->next;
+      Node * tCurr = &h;
+      while (rCurr != NULL) {
+        Node n(rCurr->data);
+        tCurr->next = &n;
+        n.prev = tCurr;
+        tCurr = &n;
+        rCurr = rCurr->next;
+      }
+      while (head != NULL) {
+        Node * temp = head->next;
+        delete head;
+        head = temp;
+      }
+      head = &h;
+      tail = tCurr;
+      size = rhs.size;
+    }
+    return *this;
+  }
+
+  int getSize() const { return size; }
+};
 
 #endif
