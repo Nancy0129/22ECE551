b26cbd9ca78043606000d5de6885161bbda924c1
xc139 <xc139@duke.edu>
2022-11-10 02:45:19 -0500

    int correct

--- 098_bst_map/test.cpp
+++ 098_bst_map/test.cpp
@@ -6,10 +6,16 @@
 
 int main(void) {
   BstMap<int, int> a;
-  a.add(1, 1);
   a.add(2, 2);
+  a.add(1, 1);
+  a.add(0, 0);
   a.add(3, 3);
-  int b = a.lookup(1);
+  a.add(4, 4);
+  a.add(0, 5);
+  int b = a.lookup(0);
+  std::cout << "b is " << b << "\n";
+  a.remove(2);
+  b = a.lookup(1);
   std::cout << "b is " << b << "\n";
   return 0;
 }
