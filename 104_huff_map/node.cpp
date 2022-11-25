f6e553975f4507be965f01593d5f5199a54589e7
xc139 <xc139@duke.edu>
2022-11-24 19:53:40 -0500

    104

--- 104_huff_map/node.cpp
+++ 104_huff_map/node.cpp
@@ -1,5 +1,15 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
-  //WRITE ME!
-}
 
+#include <cassert>
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
+  //WRITE ME!
+  if (sym != NO_SYM) {
+    assert(left == NULL && right == NULL);
+    theMap[sym] = b;
+  }
+  else {
+    assert(left != NULL && right != NULL);
+    left->buildMap(b.plusZero(), theMap);
+    right->buildMap(b.plusOne(), theMap);
+  }
+}
